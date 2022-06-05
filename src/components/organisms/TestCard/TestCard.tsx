984ff61d71c354b4af35afdf69ac613945e04bd2
abeshi03 <k_913411@yahoo.co.jp>
2022-06-05 18:00:03 +0900

    主要コンポーネントを追加

---
+++ src/components/organisms/TestCard/TestCard.tsx
@@ -0,0 +1,35 @@
+/* --- lib ----------------------------------------------------------------------------------------------------------- */
+import React, { memo, FC } from "react";
+
+/* --- businessRule -------------------------------------------------------------------------------------------------- */
+import { Test } from "../../../businessRules/Test";
+
+/* --- assets -------------------------------------------------------------------------------------------------------- */
+import styles from "./TestCard.module.scss";
+import noImage from "../../../../../public/noImage.png"
+
+type Props = {
+  test: Test
+}
+
+export const TestCard: FC<Props> = memo((props) => {
+  const { test } = props;
+  return (
+    <div className={styles.testCard}>
+      <div
+        className={styles.img}
+        style={{backgroundImage: `url(${test.thumbnailUri ?? noImage.src})`}}
+      >
+      </div>
+      <div className={styles.name}>テスト名が入ります</div>
+      <div className={styles.numberOfQuestions}>問題数: {test.numberOfQuestions}</div>
+      <div className={styles.categoriesFlow}>
+        {test.categories.map((category) => (
+          <div key={category.id} className={styles.categoryBadge}>
+            <div className={styles.categoryName}>{category.name}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+});
--- src/pages/tests/index.page.tsx
+++ src/pages/tests/index.page.tsx
@@ -1,6 +1,18 @@
-const TestsListPage = () => {
+/* --- lib ---------------------------------------------------------------------------------------------------------- */
+import type { NextPage } from "next";
+
+/* --- assets -------------------------------------------------------------------------------------------------------- */
+import styles from "./testsListPage.module.scss";
+
+/* --- component ----------------------------------------------------------------------------------------------------- */
+import { TestCard } from "../../components/organisms/TestCard/TestCard";
+
+const TestsListPage: NextPage = () => {
   return (
-    <div>テストペーゾ</div>
+    <div className={styles.testsListPage}>
+      <div className={styles.testCardsFlow}>
+      </div>
+    </div>
   )
 }
 
