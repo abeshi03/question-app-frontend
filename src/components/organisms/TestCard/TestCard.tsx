8739b7cd177cd499b4400114be9417598fcbfb83
abeshi03 <k_913411@yahoo.co.jp>
2022-06-06 21:38:18 +0900

    テスト受講ページの導線を設定

--- src/components/organisms/TestCard/TestCard.tsx
+++ src/components/organisms/TestCard/TestCard.tsx
@@ -1,5 +1,6 @@
 /* --- lib ----------------------------------------------------------------------------------------------------------- */
 import React, { memo, FC } from "react";
+import Link from "next/link";
 
 /* --- businessRule -------------------------------------------------------------------------------------------------- */
 import { Test } from "../../../businessRules/Test";
@@ -8,6 +9,9 @@ import { Test } from "../../../businessRules/Test";
 import styles from "./TestCard.module.scss";
 import noImage from "../../../../public/noImage.png";
 
+/* --- routing ------------------------------------------------------------------------------------------------------- */
+import { pagesPath } from "../../../lib/$path";
+
 type Props = {
   test: Test
 }
@@ -15,21 +19,23 @@ type Props = {
 export const TestCard: FC<Props> = memo((props) => {
   const { test } = props;
   return (
-    <div className={styles.testCard}>
-      <div
-        className={styles.img}
-        style={{backgroundImage: `url(${test.thumbnailUri ?? noImage.src})`}}
-      >
-      </div>
-      <div className={styles.name}>テスト名が入ります</div>
-      <div className={styles.numberOfQuestions}>問題数: {test.numberOfQuestions}</div>
-      <div className={styles.categoriesFlow}>
-        {test.categories.map((category) => (
-          <div key={category.id} className={styles.categoryBadge}>
-            <div className={styles.categoryName}>{category.name}</div>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Link href={pagesPath.tests._id(test.id).$url()}>
+      <a className={styles.testCard}>
+        <div
+          className={styles.img}
+          style={{backgroundImage: `url(${test.thumbnailUri ?? noImage.src})`}}
+        >
+        </div>
+        <div className={styles.name}>テスト名が入ります</div>
+        <div className={styles.numberOfQuestions}>問題数: {test.numberOfQuestions}</div>
+        <div className={styles.categoriesFlow}>
+          {test.categories.map((category) => (
+            <div key={category.id} className={styles.categoryBadge}>
+              <div className={styles.categoryName}>{category.name}</div>
+            </div>
+          ))}
+        </div>
+      </a>
+    </Link>
   );
 });
---
+++ src/pages/tests/[id]/index.page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TestTakingPage: React.FC = () => {
+  return (
+    <div>テスト</div>
+  )
+}
+
+export default TestTakingPage;
