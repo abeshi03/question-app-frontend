5c5b695b76212b1d741f3f0836a1534e054a7670
abeshi03 <k_913411@yahoo.co.jp>
2022-06-07 22:53:26 +0900

    InputFieldコンポーネントを追加

---
+++ src/components/molecules/controlls/InputField/InputField.tsx
@@ -0,0 +1,62 @@
+/* --- lib ---------------------------------------------------------------------------------------------------------- */
+import React, { memo, FC } from "react";
+
+
+/* --- assets -------------------------------------------------------------------------------------------------------- */
+import styles from "./InputField.module.scss";
+
+/* --- component ----------------------------------------------------------------------------------------------------- */
+import { InputLabel } from "../../../atoms/InputLabel/InputLabel";
+
+type InputFiledType = "text" | "password" | "email" | "number";
+
+type Props = {
+  label?: string;
+  placeholder?: string;
+  type: InputFiledType;
+  required: boolean;
+  guidance?: string;
+  disabled?: boolean;
+  defaultValue?: string;
+  autoComplete?: string;
+  className?: string;
+  inputProps: React.HTMLAttributes<HTMLInputElement>;
+}
+
+
+export const InputField: FC<Props> = memo((props) => {
+
+  const {
+    label,
+    placeholder,
+    type,
+    required,
+    guidance,
+    disabled = false,
+    defaultValue,
+    autoComplete,
+    className,
+    inputProps
+  } = props;
+
+  return (
+    <div className={styles.inputFieldContainer}>
+
+      <InputLabel
+        required={required}
+        label={label}
+      />
+
+      <input
+        { ...inputProps }
+        className={`${className} ${styles.inputField}`}
+        defaultValue={defaultValue}
+        type={type}
+        placeholder={placeholder}
+        disabled={disabled}
+        autoComplete={autoComplete}
+      />
+      {guidance && <p className={styles.guidance}>{guidance}</p>}
+    </div>
+  );
+});
